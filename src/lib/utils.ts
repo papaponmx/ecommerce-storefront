4f7e952992573b8e07fb10ea99f81302ffe414b1
Jaime Rios <jaime.rios@hey.com>
2024-08-06 17:29:19 -0600

    Refactor fetchProducts function and add getProductById function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,20 @@
 import prisma from "./db"
 
 export async function fetchProducts() {
-    try {
-        console.log('🚪 fetching products', prisma)
-        const products = await prisma.product.findMany()
-        console.log('🔥 products', products)
-        return { props: products }
-    } catch (error) {
-        console.log('🖼️ error', error)
-        throw new Error('Failed to fetch products')
-    }
+  try {
+    console.log('🚪 fetching products', prisma)
+    const products = await prisma.product.findMany()
+    console.log('🔥 products', products.length)
+    return { props: products }
+  } catch (error) {
+    console.log('🖼️ error', error)
+    throw new Error('Failed to fetch products')
+  }
+}
+
+export async function getProductById(id: string) {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+  return product;
 }
